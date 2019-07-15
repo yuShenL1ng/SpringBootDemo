3841656edcf73ad13d0d8b0e15835b5ba5d09335
yuShenL1ng <qwe1993509QWE>
2019-07-15 11:53:21 +0800

    上传新增

--- SpringMybatis/src/main/java/com/example/demo/controller/SalesorderDemoController.java
+++ SpringMybatis/src/main/java/com/example/demo/controller/SalesorderDemoController.java
@@ -1,18 +1,25 @@
 package com.example.demo.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.dao.SalesorderDemoDao;
+import com.example.demo.model.ResponseInfo;
 import com.example.demo.model.SalesorderDemo;
 import com.example.demo.service.SalesorderDemoService;
+import com.example.demo.util.Uploadfiles;
 
 @Controller
 @RequestMapping("/SalesorderDemo")
@@ -47,5 +54,18 @@ public class SalesorderDemoController {
 	public int deleteSalesorderDemo(HttpServletRequest request,@RequestBody SalesorderDemo salesorderDemo) {
 		
 		return SalesorderDemoService.deleteSalesorderDemo(salesorderDemo);
+	}
+	
+	@RequestMapping("/upload.action")
+	@ResponseBody
+	public ResponseInfo upload(@RequestParam("file") MultipartFile[] myfiles, HttpServletRequest request, HttpServletResponse response) {
+		ResponseInfo info = new ResponseInfo();
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+/*		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");//设置日期格式
+		String date = df.format(new Date());// new Date()为获取当前系统时间，也可使用当前时间戳
+*/		String realPath ="D:\\springBootUpload\\image\\"+name;
+		info = Uploadfiles.uploadfiles(myfiles, realPath);
+		return info;
 	}
 }
--- SpringMybatis/src/main/java/com/example/demo/model/ResponseInfo.java
+++ SpringMybatis/src/main/java/com/example/demo/model/ResponseInfo.java
@@ -1,51 +1,38 @@
 package com.example.demo.model;
 
+/**
+ * 返回信息
+ */
+public class ResponseInfo {
 
-public class ResponseInfo{
-
-    /**
-     * 返回信息
-     */
-   // private static final long serialVersionUID = -8918590349400215878L;
-    private String code;
-    private String message;
-    private String data;
-    private String amount;
-    private int sid;
-    
-    
-
-    public int getSid() {
-		return sid;
-	}
-
-	public void setSid(int sid) {
-		this.sid = sid;
-	}
+	private String code;
+	private String message;
+	private String data;
+	private String amount;
 
 	public String getCode() {
-        return code;
-    }
+		return code;
+	}
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+	public void setCode(String code) {
+		this.code = code;
+	}
 
-    public String getMessage() {
-        return message;
-    }
+	public String getMessage() {
+		return message;
+	}
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
-    public String getData() {
-        return data;
-    }
+	public String getData() {
+		return data;
+	}
 
-    public void setData(String data) {
-        this.data = data;
-    }
+	public void setData(String data) {
+		this.data = data;
+	}
 
 	public String getAmount() {
 		return amount;
